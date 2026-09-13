ec2ee873ee8a90950ebe7937cf8f17fc19317e9a
Spondan Bandyopadhyay <126954192+SpondanB@users.noreply.github.com>
2026-09-13 17:14:45 +0100

    Create bigram.py

---
+++ bigram.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# hyperparameters
+batch_size = 32
+block_size = 8
+max_iters = 6000
+eval_interval = 300
+learning_rate = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+
+
+# for reproducibility, we set the random seed to a fixed value.
+# torch.manual_seed(1337)
+
+# reading the input data.
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# creating the set of unique characters that are present in the text.
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+# to tokenize the text, we can use the following code:
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# putting the data into a tensor.
+data = torch.tensor(encode(text), dtype=torch.long)
+
+# making the training and test data.
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# creating the batches of the data - data loader
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+# creating a function to estimate the loss on the train and val sets. 
+# it is important to note that we are using the @torch.no_grad() decorator to disable gradient calculation, since we are only interested in the loss values and not in updating the model parameters.
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+# defining a simple Bigram language model.
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (Batch ,Time ,Channels)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+
+            loss = F.cross_entropy(logits, targets) # basically we want to measure the loss between the predicted logits and the actual targets. We use cross-entropy loss for this purpose.
+            # pytorch's cross_entropy function expects the input to be of shape (N, C) where N is the number of samples and C is the number of classes. So we reshape the logits and targets to be of shape (B*T, vocab_size) and (B*T,) respectively.
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx, targets=None)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+# setting up the PyTorch optimizer.
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+
+
+for iter in range(max_iters):
+
+    # check the loss on train and val sets every eval_interval iterations.
+    if iter % eval_interval == 0 :
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist())) # generate 500 new characters from the model
