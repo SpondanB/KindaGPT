6fdce176032316eed8d466c77850c46afe7ba27f
Spondan Bandyopadhyay <126954192+SpondanB@users.noreply.github.com>
2026-09-13 18:06:55 +0100

    code update for the self attention section

--- bigram.py
+++ bigram.py
@@ -10,6 +10,7 @@ eval_interval = 300
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
 
 
 # for reproducibility, we set the random seed to a fixed value.
@@ -67,14 +68,21 @@ def estimate_loss():
 # defining a simple Bigram language model.
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # this is a lookup table that maps each token to a vector of size n_embd. The embedding layer takes in the vocab_size and n_embd as input and creates a matrix of size (vocab_size, n_embd) where each row corresponds to a token and each column corresponds to a dimension of the embedding space.
+        self.position_embedding_table = nn.Embedding(block_size, n_embd) # this is a lookup table that maps each position in the input sequence to a vector of size n_embd. The embedding layer takes in the block_size and n_embd as input and creates a matrix of size (block_size, n_embd) where each row corresponds to a position in the input sequence and each column corresponds to a dimension of the embedding space.
+        self.lm_head = nn.Linear(n_embd, vocab_size) # this is a linear layer that takes in the embedding vector and outputs a vector of size vocab_size. The linear layer takes in the n_embd and vocab_size as input and creates a matrix of size (n_embd, vocab_size) where each row corresponds to a dimension of the embedding space and each column corresponds to a token.
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape
+
         # idx and targets are both (B,T) tensor of integers
-        logits = self.token_embedding_table(idx) # (Batch ,Time ,Channels)
+        tok_embed = self.token_embedding_table(idx) # (Batch ,Time ,Channels)
+        pos_embed = self.position_embedding_table(torch.arange(T, device=device)) # (Time ,Channels)
+        x = tok_embed + pos_embed # (Batch ,Time ,Channels)
+        logits = self.lm_head(x) # (Batch ,Time ,Vocab_Size)
 
         if targets is None:
             loss = None
@@ -104,7 +112,7 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # setting up the PyTorch optimizer.
